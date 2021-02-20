d08e38fbaf349844b91b1c5ada14390457ebb40f
Quang Tuyen DINH <qtuyen43@gmail.com>
2021-02-20 20:12:15 +0100

    Created interface Album, successfully fetch API and map array

--- bdl-dinh-quang-tuyen/src/model/albums.tsx
+++ bdl-dinh-quang-tuyen/src/model/albums.tsx
@@ -1,1 +1,7 @@
-import React from 'react'
+const dataUrl = "https://itunes.apple.com/us/rss/topalbums/limit=100/json"
+
+export const fetchAlbumsData = async () => {
+  const response = await fetch(dataUrl);
+  const data = await response.json();
+  return data.feed.entry;
+}
